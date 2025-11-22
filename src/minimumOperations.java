f077a7a1462124e6f37e8e7d0abf1c77de25af4c
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-11-22 14:05:12 +0200

    22/11/2025

--- src/minimumOperations.java
+++ src/minimumOperations.java
@@ -1,6 +1,7 @@
-import java.util.Arrays;
 
+//problem:https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three?envType=daily-question&envId=2025-11-22
 public class minimumOperations {
+
     public int soluation (int []nums){
         int sum =0;
         for(int i=0;i<nums.length;i++){
