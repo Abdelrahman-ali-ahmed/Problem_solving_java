6c21473577693c0f79ca050ab18f9a3d9162a21d
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-11-25 16:45:06 +0200

    25/11/2025

---
+++ src/smallestRepunitDivByK.java
@@ -0,0 +1,12 @@
+public class smallestRepunitDivByK {
+    public int solution(int k){
+        if(k%2==0||k%5==0) return -1;
+        int r=1%k;
+        int len=1;
+        while(r!=0){
+            r=(r*10+1)%k;
+            len++;
+        }
+        return len;
+    }
+}
